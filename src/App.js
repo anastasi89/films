80a6ca2a203b5a7836650be7364538af40ec74e2
Внукова Анастасия <nastenka696945@gmail.com>
2023-09-15 19:07:32 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Stars from './components/Stars';
+
+function App() {
+  return (
+    <div className='rating-list'>
+      <Stars count={1} />
+      <Stars count={6} />
+      <Stars count={3} />
+      <Stars count={4} />
+      <Stars count={5} />
+    </div>
+  );
+}
+
+export default App;
